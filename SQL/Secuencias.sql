57d9f331a348c912fd4aad0179c76fc99c5ef9ec
Ludwig <theludway@gmail.com>
2024-05-16 07:30:08 -0500

    Secuencias para el proyecto

---
+++ SQL/Secuencias.sql
@@ -0,0 +1,41 @@
+-- Para la secuencia de las aerolíneas
+CREATE SEQUENCE secuencia_aerolinea START WITH 1 INCREMENT BY 1;
+
+-- Para la secuencia de los sitios
+CREATE SEQUENCE secuencia_sitios START WITH 1 INCREMENT BY 1;
+
+-- Para la secuencia de los paises
+CREATE SEQUENCE secuencia_paises START WITH 1 INCREMENT BY 1;
+
+-- Para la secuencia de las ciudades
+CREATE SEQUENCE secuencia_ciudades START WITH 1 INCREMENT BY 1;
+
+-- Para la secuencia de los hoteles.
+CREATE SEQUENCE secuencia_hoteles START WITH 1 INCREMENT BY 1;
+
+-- Para la secuencia de las habitaciones.
+CREATE SEQUENCE secuencia_hoteles START WITH 1 INCREMENT BY 1;
+
+-- Para la secuencia de los bancos
+CREATE SEQUENCE secuencia_bancos START WITH 1 INCREMENT BY 1;
+
+-- Para la secuencia de los métodos de pago
+CREATE SEQUENCE secuencia_metodospago START WITH 1 INCREMENT BY 1;
+
+-- Para la secuencia de los clientes
+CREATE SEQUENCE secuencia_clientes START WITH 1 INCREMENT BY 1;
+
+-- Para la secuencia de los acompañantes
+CREATE SEQUENCE secuencia_acompanantes START WITH 1 INCREMENT BY 1;
+
+-- Para la solicitud de crédito
+CREATE SEQUENCE secuencia_credito START WITH 1 INCREMENT BY 1;
+
+-- Para la reserva de Hotel.
+CREATE SEQUENCE secuencia_reserva_hotel START WITH 1 INCREMENT BY 1;
+
+-- Para la reserva del vuelo
+CREATE SEQUENCE secuencia_reserva_vuelo START WITH 1 INCREMENT BY 1;
+
+-- Para la reserva en general
+CREATE SEQUENCE secuencia_reserva_general START WITH 1 INCREMENT BY 1;
